3aa4ef34eae99c41b2ef35ae52299b7254aa1347
EpixBakFlippz <100427193+EpixBakFlippz@users.noreply.github.com>
2022-02-25 09:12:19 -0600

    Add files via upload

---
+++ menu-descr.js
@@ -0,0 +1,83 @@
+$( document ).ready(function() {
+    $("#crssnt").hover(
+    	function(){
+    		$(".crsnt-dscr").addClass("active");
+    	},
+    	function(){
+			$(".crsnt-dscr").removeClass("active");
+    	});
+
+
+    $("#chclt-mffn").hover(
+    	function(){
+    		$(".mffn-dscr").addClass("active");
+    	},
+    	function(){
+			$(".mffn-dscr").removeClass("active");
+    	});
+
+	$("#bgls").hover(
+	    	function(){
+	    		$(".bgl-dscr").addClass("active");
+	    	},
+	    	function(){
+				$(".bgl-dscr").removeClass("active");
+	    	});
+
+	$("#bnn-brd").hover(
+	    	function(){
+	    		$(".brd-dscr").addClass("active");
+	    	},
+	    	function(){
+				$(".brd-dscr").removeClass("active");
+	    	});
+
+	$("#viet").hover(
+	    	function(){
+	    		$(".viet-dscr").addClass("active");
+	    	},
+	    	function(){
+				$(".viet-dscr").removeClass("active");
+	    	});
+
+	$("#icy-cff").hover(
+	    	function(){
+	    		$(".iced-dscr").addClass("active");
+	    	},
+	    	function(){
+				$(".iced-dscr").removeClass("active");
+	    	});
+
+	$("#ntr-cff").hover(
+	    	function(){
+	    		$(".nitro-dscr").addClass("active");
+	    	},
+	    	function(){
+				$(".nitro-dscr").removeClass("active");
+	    	});
+
+	$("#chai-latt").hover(
+	    	function(){
+	    		$(".chai-dscr").addClass("active");
+	    	},
+	    	function(){
+				$(".chai-dscr").removeClass("active");
+	    	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+});
